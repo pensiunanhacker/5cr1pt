c985c7127d7478027199e7a2288db2582471114b
pensiunanhacker <49938328+pensiunanhacker@users.noreply.github.com>
2019-10-19 00:17:44 +0800

    link raw
    top line changed

---
+++ aimbotdevlobby(upload).lua
@@ -0,0 +1,309 @@
+function Home1()
+nHome = gg.choice (
+{
+--1
+"😈 aimbot devil lobby",
+},
+nil,
+" ")
+if nHome == 1 then aimbotdevil() end
+--this code bellow just for fix error when "nil"
+if nHome == nil then else EXIT() end
+end
+-----------------------------pastecode room....
+function aimbotdevil()
+--😈aimbotdevillobby
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber("0;1.19193046077e-40;1;-9.6172094345093e-05;1.6815581571898e-44::384", gg.TYPE_DWORD, false)
+gg.searchNumber("0", gg.TYPE_DWORD, false)
+gg.getResults(10)
+gg.editAll("131120.0", gg.TYPE_DWORD)
+gg.clearResults()
+gg.toast("😈Please Wait...")
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber(" 1.0509738e-43; 5.6051939e-45; 7.0064923e-45; 4.0:13 ", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("4", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(1)
+gg.editAll("3", gg.TYPE_FLOAT)
+gg.clearResults()
+gg.toast("😈Please Wait...")
+gg.setRanges(32)
+gg.searchNumber("0.00111111114;1.0e32;49.9999961853;24.99999809265", 16, false, 536870912, 0, -1)
+gg.searchNumber("1.0e32", 16, false, 536870912, 0, -1)
+gg.getResults(20)
+gg.editAll("9", 16)
+gg.clearResults()
+pensiunanhacker = "9"
+gg.clearResults()
+BSSREGION5();
+gg.clearResults()
+BSSREGION6();
+gg.clearResults()
+BSSREGION7();
+gg.clearResults()
+BSSREGION8();
+gg.clearResults()
+PENHACK1();
+gg.clearResults()
+PENHACK2();
+gg.clearResults()
+PENHACK3();
+gg.clearResults()
+PENHACK4();
+gg.clearResults()
+os.remove( " /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Paks/puffer_temp ")
+os.remove( " /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Paks/1375135419_47_0.14.5.11184_20190924095428_548156081_cures.ifs.res ")
+os.remove( " /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Paks/filelist.json ")
+os.remove( " /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Paks/puffer_res.eifs ")
+os.remove( " /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Paks/PufferFileList.json ")
+os.remove( " /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Paks/apollo_reslist.flist ")
+os.remove( " /storage/emulated/0/Android/data/com.tencent.ig/cache ")
+os.remove( " /storage/emulated/0/Android/data/com.tencent.ig/files/ca-bundle.pem ")
+os.remove( " /storage/emulated/0/Android/data/com.tencent.ig/files/tbslog ")
+os.remove( " /storage/emulated/0/Android/data/com.tencent.ig/files/login-identifier.txt ")
+os.remove( " /storage/emulated/0/Android/data/com.tencent.ig/files/cacheFile.txt ")
+os.remove( " /storage/emulated/0/Android/data/com.tencent.ig/files/vmpcloudconfig.json ")
+os.remove( " /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Logs ")
+os.remove( " /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/RoleInfo/RoleInfo.json ")
+os.remove( " /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/UpdateInfo ")
+os.remove( " /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/ImageDownload ")
+os.remove( " /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Pandora ")
+os.remove( " /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/Engine ")
+os.remove( " /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/StatEventReportedFlag ")
+os.remove( " /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/PufferTmpDir ")
+os.remove( " /data/data/com.tencent.ig/app_appcache ")
+os.remove( " /data/data/com.tencent.ig/app_bugly ")
+os.remove( " /data/data/com.tencent.ig/app_crashrecord ")
+os.remove( " /data/data/com.tencent.ig/cache ")
+os.remove( " /data/data/com.tencent.ig/code_cache ")
+os.remove( " /data/data/com.tencent.ig/files/iMSDK ")
+os.remove( " /data/data/com.tencent.ig/files/ss_tmp ")
+os.remove( " /data/data/com.tencent.ig/files/tss_tmp ")
+os.remove( " /data/data/com.tencent.ig/files/AppEventsLogger.persistedevents ")
+os.remove( " /data/data/com.tencent.ig/files/tpnlcache.data ")
+os.remove( " /data/data/com.tencent.ig/files/tss_app_915c.dat ")
+os.remove( " /data/data/com.tencent.ig/files/tss_cs_stat2.dat ")
+os.remove( " /data/data/com.tencent.ig/files/tss.i.m.dat ")
+os.remove( " /storage/emulated/0/Android/data/com.tencent.ig/cache ")
+os.remove( " /storage/emulated/0/Android/data/com.tencent.ig/files/tbslog ")
+os.remove( " /storage/emulated/0/Android/data/com.tencent.ig/files/ca-bundle.pem ")
+os.remove( " /storage/emulated/0/Android/data/com.tencent.ig/files/cacheFile.txt ")
+os.remove( " /storage/emulated/0/Android/data/com.tencent.ig/files/login-identifier.txt ")
+os.remove( " /storage/emulated/0/Android/data/com.tencent.ig/files/vmpcloudconfig.json ")
+os.remove( " /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Paks/puffer_temp ")
+os.remove( " /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Paks/puffer_res.eifs ")
+os.remove( " /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Logs ")
+os.remove( " /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/UpdateInfo ")
+os.remove( " /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/RoleInfo ")
+os.remove( " /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Pandora ")
+os.remove( " /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/PufferTmpDir ")
+os.remove( " /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/UpdateInfo ")
+os.remove( " /storage/emulated/0/tencent ")
+os.remove( " /data/data/com.tencent.ig/app_appcache ")
+os.remove( " /data/data/com.tencent.ig/app_bugly ")
+os.remove( " /data/data/com.tencent.ig/cache ")
+os.remove( " /data/data/com.tencent.ig/code_cache ")
+os.remove( " /data/data/com.tencent.ig/app_crashrecord ")
+os.remove( " /data/data/com.tencent.ig/databases/google_app_measurement_local.db-journal ")
+os.remove( " /data/data/com.tencent.ig/databases/pri_tencent_analysis.db-journal ")
+os.remove( " /data/data/com.tencent.ig/databases/tencent_analysis.db-journal ")
+os.remove( " /data/data/com.tencent.ig/databases/xg_message.db-journal ")
+os.remove( " /data/data/com.tencent.ig/databases/google_app_measurement_local.db ")
+os.remove( " /data/data/com.tencent.ig/databases/pri_tencent_analysis.db ")
+os.remove( " /data/data/com.tencent.ig/databases/__hs__db_issues-journal ")
+os.remove( " /data/data/com.tencent.ig/databases/__hs__db_key_values-journal ")
+os.remove( " /data/data/com.tencent.ig/databases/__hs__db_sessions-journal ")
+os.remove( " /data/data/com.tencent.ig/databases/__hs__db_support_key_values-journal ")
+os.remove( " /data/data/com.tencent.ig/databases/bugly_db_-journal ")
+os.remove( " /data/data/com.tencent.ig/databases/__hs_log_store-journal ")
+os.remove( " /data/data/com.tencent.ig/files/iMSDK ")
+os.remove( " /data/data/com.tencent.ig/files/ss_tmp ")
+os.remove( " /data/data/com.tencent.ig/files/tss_tmp ")
+os.remove( " /data/data/com.tencent.ig/files/AppEventsLogger.persistedevents ")
+os.remove( " /data/data/com.tencent.ig/files/tpnlcache.data ")
+os.remove( " /data/data/com.tencent.ig/files/tss_app_915c.dat ")
+os.remove( " /data/data/com.tencent.ig/files/tss_cs_stat2.dat ")
+os.remove( " /data/data/com.tencent.ig/files/tss.i.m.dat ")
+gg.clearResults()
+end
+-----------------------------------------------------------------------
+function PENHACK1()
+  gg.toast("😈Please Wait...")
+  gg.setRanges(gg.REGION_C_ALLOC)
+  gg.searchNumber("3.0262028e29;1.2926333e22;1.6927603e22;1.1942677e30;4.6304261e27;1.6927535e22;6.7996369e22;1.7851485e25;2.2230891e-10;4.5245454e30;2.0773937e-10;0.00017089097;1.8521706e28;1.1483318e27:57", gg.TYPE_FLOAT)
+  local t = gg.getResults(20)
+  do
+    do
+      for _FORV_4_, _FORV_5_ in ipairs(t) do
+        t[_FORV_4_].value = "0"
+        t[_FORV_4_].freeze = true
+      end
+    end
+  end
+  --   gg.addListItems(t)
+end
+function PENHACK2()
+  gg.toast("😈Please Wait...")
+  gg.setRanges(gg.REGION_C_ALLOC)
+  gg.searchNumber("6.8894428e22;8.6256162e29;7.0309241e28:13", gg.TYPE_FLOAT)
+  local t = gg.getResults(20)
+  do
+    do
+      for _FORV_4_, _FORV_5_ in ipairs(t) do
+        t[_FORV_4_].value = "0"
+        t[_FORV_4_].freeze = true
+      end
+    end
+  end
+  --   gg.addListItems(t)
+end
+function PENHACK3()
+  gg.toast("😈Please Wait...")
+  gg.setRanges(gg.REGION_ANONYMOUS)
+  gg.searchNumber("2.3509887e-38;7.745564e31;1.5936918e-10;6.8894428e22;8.6256162e29;7.0309241e28;8.9324369e-41:49", gg.TYPE_FLOAT)
+  gg.refineNumber("2.3509887e-38;8.9324369e-41", gg.TYPE_FLOAT)
+  local t = gg.getResults(3)
+  do
+    do
+      for _FORV_4_, _FORV_5_ in ipairs(t) do
+        t[_FORV_4_].value = "0"
+        t[_FORV_4_].freeze = true
+      end
+    end
+  end
+  --   gg.addListItems(t)
+end
+function PENHACK4()
+  gg.toast("😈Please Wait...")
+  gg.setRanges(gg.REGION_ANONYMOUS)
+  gg.searchNumber("1.102786e27;3.3236012e-9;5.489758e-11;1.0050027e-38:13", gg.TYPE_FLOAT)
+  local t = gg.getResults(20)
+  do
+    do
+      for _FORV_4_, _FORV_5_ in ipairs(t) do
+        t[_FORV_4_].value = "0"
+        t[_FORV_4_].freeze = true
+      end
+    end
+  end
+  --   gg.addListItems(t)
+end
+-----------------------------------------------------------------------
+--freeze BSSregion
+function BSSREGION5()
+  gg.toast("😈Please Wait... will frezee all BSS region")
+  gg.setRanges(gg.REGION_C_BSS)
+  gg.searchNumber("16,777,216;1,953,785,899;791,624,304;1,701,407,844;1,898,852,708;1,868,770,929;1,701,392,237;1,717,662,564;1,816,342,373;1,953,391,977;1,684,955,439;63,744:49", gg.TYPE_DWORD)
+  gg.refineNumber("16,777,216;63,744:49", gg.TYPE_DWORD)
+  local t = gg.getResults(20)
+  do
+    do
+      for _FORV_4_, _FORV_5_ in ipairs(t) do
+        t[_FORV_4_].value = "0"
+        t[_FORV_4_].freeze = true
+      end
+    end
+  end
+  --   gg.addListItems(t)
+end
+function BSSREGION6()
+  gg.toast("😈Please Wait... will frezee all BSS region")
+  gg.setRanges(gg.REGION_C_BSS)
+  gg.searchNumber("1,818,496,257;828,663,145;779,186,478:13", gg.TYPE_DWORD)
+  local t = gg.getResults(20)
+  do
+    do
+      for _FORV_4_, _FORV_5_ in ipairs(t) do
+        t[_FORV_4_].value = "0"
+        t[_FORV_4_].freeze = true
+      end
+    end
+  end
+  --   gg.addListItems(t)
+end
+function BSSREGION7()
+  gg.toast("😈Please Wait... will frezee all BSS region")
+  gg.setRanges(gg.REGION_C_BSS)
+  gg.searchNumber("2.3509887e-38;7.745564e31;1.5936918e-10;6.8894428e22;8.6256162e29;7.0309241e28;8.9324369e-41:49", gg.TYPE_FLOAT)
+  gg.refineNumber("2.3509887e-38;8.9324369e-41", gg.TYPE_FLOAT)
+  local t = gg.getResults(3)
+  do
+    do
+      for _FORV_4_, _FORV_5_ in ipairs(t) do
+        t[_FORV_4_].value = "0"
+        t[_FORV_4_].freeze = true
+      end
+    end
+  end
+  --   gg.addListItems(t)
+end
+function BSSREGION8()
+  gg.toast("😈Please Wait... will frezee all BSS region")
+  gg.setRanges(gg.REGION_C_BSS)
+  gg.searchNumber("1.102786e27;3.3236012e-9;5.489758e-11;1.0050027e-38:13", gg.TYPE_FLOAT)
+  local t = gg.getResults(20)
+  do
+    do
+      for _FORV_4_, _FORV_5_ in ipairs(t) do
+        t[_FORV_4_].value = "0"
+        t[_FORV_4_].freeze = true
+      end
+    end
+  end
+  --   gg.addListItems(t)
+end
+-----------------------------------------------------------------------
+--function FOOTER AND EXIT bellow
+function EXIT()
+gg.setVisible(false) --false to hide -- true to view
+--there are 15 memory range on your device
+--added 1
+--green
+gg.setRanges(
+gg.REGION_JAVA_HEAP |
+gg.REGION_C_HEAP |
+gg.REGION_C_ALLOC |
+gg.REGION_C_DATA |
+gg.REGION_C_BSS |
+gg.REGION_PPSSPP |
+gg.REGION_ANONYMOUS |
+--yellow
+gg.REGION_JAVA |
+gg.REGION_STACK |
+gg.REGION_ASHMEM |
+--yellowlight
+gg.REGION_OTHER |
+--red
+gg.REGION_BAD |
+--purple
+gg.REGION_CODE_APP |
+--purple_light
+gg.REGION_CODE_SYS |
+--added
+gg.REGION_VIDEO)
+-----------------------------------------------------------------------
+--function skipRestore for restore...
+--your setting memory saved
+gg.skipRestoreState()
+-----------------------------------------------------------------------
+--code exit from your operating system
+os.exit()
+end 
+-----------------------------------------------------------------------
+--function HOME yout can put it on top too
+function HOME()
+anythingyoucanwritehere=1
+Home1()
+end
+-----------------------------------------------------------------------
+--PH letter is your code menu script
+while(true)do
+if gg.isVisible(true) then PH=1
+gg.setVisible(false)
+end
+-----------------------------------------------------------------------
+if PH==1 then Home1() os.exit() end
+end
+-----------------------------------------------------------------------
+
+
