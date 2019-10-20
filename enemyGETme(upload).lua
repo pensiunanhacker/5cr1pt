28a17dc977be609aad2d21651fd3c80c895b146c
pensiunanhacker <49938328+pensiunanhacker@users.noreply.github.com>
2019-10-21 01:26:53 +0800

    Add files via upload

---
+++ enemyGETme(upload).lua
@@ -0,0 +1,113 @@
+--p.e.n.s.i.u.n.a.n.h.a.c.k.e.r
+function Home1()
+nHome = gg.choice (
+{
+--1
+"👢 enemyGETme",
+},
+nil,
+" ")
+if nHome == 1 then enemyGETme() end
+--this code bellow just for fix error when "nil"
+if nHome == nil then else EXIT() end
+end
+-----------------------------pastecode room....
+function enemyGETme()
+gg.toast("loading... please wait...")
+gg.toast("║▓░░░░░░░░░░░░░░║")
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber("1;35;443;55;0.57357645035", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("1", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(333)
+gg.editAll("3", gg.TYPE_FLOAT)
+gg.clearResults()
+gg.toast("║▓▓░░░░░░░░░░░░░║")
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber("0;7.0064923e-45;1;100;1;2,500,000,000.0;0.10000000149;88", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("1", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(222)
+gg.editAll("2222", gg.TYPE_FLOAT)
+gg.clearResults()
+gg.toast("║▓▓▓░░░░░░░░░░░░║")
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber("1400;0.10000000149;1000;88;60;30", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("60", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(6666)
+gg.editAll("-666", gg.TYPE_FLOAT)
+gg.clearResults()
+gg.toast("║▓▓▓▓░░░░░░░░░░░║")
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber("3;35;443;55;0.57357645035", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("443", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(443)
+gg.editAll("4433", gg.TYPE_FLOAT)
+gg.clearResults()
+
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber("1.0F; -0.70710676908F; 0.70710670948F; 64.0F; 1.793662e-43F;1.4012985e-45F; 1D; 1D ::", gg.TYPE_FLOAT, false)
+gg.getResults(512)
+gg.refineNumber("1.4012985e-45", gg.TYPE_FLOAT, false)
+gg.getResults(51)
+gg.editAll("512512512", gg.TYPE_FLOAT)
+gg.clearResults()
+gg.toast("║▓▓▓▓▓░░░░░░░░░░║")
+gg.toast("║▓▓▓▓▓░👢░░░░░░░░║")
+gg.toast("║▓▓▓▓▓░░░👢░░░░░░║")
+gg.toast("║▓▓▓▓▓░░░░░👢░░░░║")
+gg.toast("║▓▓▓▓▓▓▓▓▓▓👢 DONE ░║")
+end
+-----------------------------------------------------------------------
+--function FOOTER AND EXIT bellow
+function EXIT()
+gg.setVisible(false) --false to hide -- true to view
+--there are 15 memory range on your device
+--added 1
+--green
+gg.setRanges(
+gg.REGION_JAVA_HEAP |
+gg.REGION_C_HEAP |
+gg.REGION_C_ALLOC |
+gg.REGION_C_DATA |
+gg.REGION_C_BSS |
+gg.REGION_PPSSPP |
+gg.REGION_ANONYMOUS |
+--yellow
+gg.REGION_JAVA |
+gg.REGION_STACK |
+gg.REGION_ASHMEM |
+--yellowlight
+gg.REGION_OTHER |
+--red
+gg.REGION_BAD |
+--purple
+gg.REGION_CODE_APP |
+--purple_light
+gg.REGION_CODE_SYS |
+--added
+gg.REGION_VIDEO)
+-----------------------------------------------------------------------
+--function skipRestore for restore...
+--your setting memory saved
+gg.skipRestoreState()
+-----------------------------------------------------------------------
+--code exit from your operating system
+os.exit()
+end 
+-----------------------------------------------------------------------
+--function HOME yout can put it on top too
+function HOME()
+anythingyoucanwritehere=1
+Home1()
+end
+-----------------------------------------------------------------------
+--PH letter is your code menu script
+while(true)do
+if gg.isVisible(true) then PH=1
+gg.setVisible(false)
+end
+-----------------------------------------------------------------------
+if PH==1 then Home1() os.exit() end
+end
+-----------------------------------------------------------------------
+
+
