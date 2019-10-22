371b23ce85d5384f08fcf5af16b7b1292bbff66e
pensiunanhacker <49938328+pensiunanhacker@users.noreply.github.com>
2019-10-22 23:31:10 +0800

    Add files via upload

---
+++ fixWHsnapPUBG15(upload).lua
@@ -0,0 +1,116 @@
+--p.e.n.s.i.u.n.a.n.h.a.c.k.e.r
+function Home1()
+nHome = gg.choice (
+{
+--1
+"☑ FIX UR SNAPS COLOUR   \n[CLICK FIX]",
+},
+nil,
+" ")
+if nHome == 1 then codeSD() end
+--this code bellow just for fix error when "nil"
+if nHome == nil then else EXIT() end
+end
+-----------------------------pastecode room....
+--⛄
+function codeSD()
+gg.clearResults()
+gg.clearResults()
+gg.clearResults()
+CODEbyAdminSnow =gg.searchNumber
+pensiunanhacker =gg.clearResults()
+gg.setRanges(gg.REGION_BAD)
+gg.searchNumber("5.6447121e21;-8.3252823e-40;4.9252852e21::", 16, false, 536870912, 0, -1)
+gg.searchNumber("-8.3252823e-40", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(99)
+gg.editAll("6454", gg.TYPE_FLOAT)
+gg.clearResults()
+gg.setRanges(gg.REGION_BAD)
+gg.searchNumber("4.9068373e21;-3.5875931e-39;4.8699618e21::", 16, false, 536870912, 0, -1)
+gg.searchNumber("-3.5875931e-39", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(99)
+gg.editAll("6454", gg.TYPE_FLOAT)
+gg.clearResults()
+gg.setRanges(gg.REGION_BAD)
+gg.searchNumber("5.0544113e21;-3.4039221e-39;4.8699607e21::", 16, false, 536870912, 0, -1)
+gg.searchNumber("-3.4039221e-39", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(99)
+gg.editAll("6454", gg.TYPE_FLOAT)
+gg.clearResults()
+gg.setRanges(gg.REGION_BAD)
+gg.searchNumber("5.6447206e21;-1.0161992e-39;4.9068396e21::", 16, false, 536870912, 0, -1)
+gg.searchNumber("-1.0161992e-39", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(99)
+gg.editAll("6454", gg.TYPE_FLOAT)
+gg.clearResults()
+gg.setRanges(gg.REGION_BAD)
+gg.searchNumber("4.7223665e21;-8.3246237e-40;4.8330515e21::", 16, false, 536870912, 0, -1)
+gg.searchNumber("-8.3246237e-40", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(99)
+gg.editAll("6454", gg.TYPE_FLOAT)
+gg.clearResults()
+gg.setRanges(gg.REGION_BAD)
+gg.searchNumber("4.7408149e21;-5.5695588e-40;4.814603e21::", 16, false, 536870912, 0, -1)
+gg.searchNumber("-5.5695588e-40", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(6)
+gg.editAll("6454", gg.TYPE_FLOAT)
+gg.clearResults()
+gg.clearResults()
+gg.clearResults()
+gg.alert("☑DONE... UR COLOUR FIX WH \n(next go to wh menu)")
+end
+-----------------------------------------------------------------------
+--function FOOTER AND EXIT bellow
+function EXIT()
+gg.setVisible(false) --false to hide -- true to view
+--there are 15 memory range on your device
+--added 1
+--green
+gg.setRanges(
+gg.REGION_JAVA_HEAP |
+gg.REGION_C_HEAP |
+gg.REGION_C_ALLOC |
+gg.REGION_C_DATA |
+gg.REGION_C_BSS |
+gg.REGION_PPSSPP |
+gg.REGION_ANONYMOUS |
+--yellow
+gg.REGION_JAVA |
+gg.REGION_STACK |
+gg.REGION_ASHMEM |
+--yellowlight
+gg.REGION_OTHER |
+--red
+gg.REGION_BAD |
+--purple
+gg.REGION_CODE_APP |
+--purple_light
+gg.REGION_CODE_SYS |
+--added
+gg.REGION_VIDEO)
+-----------------------------------------------------------------------
+--function skipRestore for restore...
+--your setting memory saved
+gg.skipRestoreState()
+-----------------------------------------------------------------------
+--code exit from your operating system
+os.exit()
+end 
+-----------------------------------------------------------------------
+--function HOME yout can put it on top too
+function HOME()
+anythingyoucanwritehere=1
+Home1()
+end
+-----------------------------------------------------------------------
+--PH letter is your code menu script
+while(true)do
+if gg.isVisible(true) then PH=1
+gg.setVisible(false)
+end
+-----------------------------------------------------------------------
+if PH==1 then Home1() os.exit() end
+end
+-----------------------------------------------------------------------
+
+
