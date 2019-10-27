7d9088ab410227a21253520f8258979f30008430
pensiunanhacker <49938328+pensiunanhacker@users.noreply.github.com>
2019-10-28 03:50:44 +0800

    Add files via upload

---
+++ speedNOstep(upload).lua
@@ -0,0 +1,106 @@
+--p.e.n.s.i.u.n.a.n.h.a.c.k.e.r
+function Home1()
+nHome = gg.choice (
+{
+--1
+"👣 speedNOstep",
+},
+nil,
+" ")
+if nHome == 1 then speedNOstep() end
+--this code bellow just for fix error when "nil"
+if nHome == nil then else EXIT() end
+end
+-----------------------------pastecode room....
+function speedNOstep()
+--Decrease Fall
+gg.clearResults()
+gg.setRanges(32)
+gg.searchNumber("1.0F;0.6;0.1;0.125F::55", 16, false, 536870912, 0, -1)
+gg.searchNumber("1", 16, false, 536870912, 0, -1)
+gg.getResults(100)
+gg.editAll("1.3", 16)
+gg.clearResults()
+--No Foot Step 
+gg.clearResults()
+gg.setRanges(32)
+gg.setVisible(false)
+gg.searchNumber("2D;256D;256D;0.96666663885117;256D", 16, false, 536870912, 0, -1)
+gg.setVisible(false)
+gg.searchNumber("0.96666663885117", 16, false, 536870912, 0, -1)
+gg.getResults(3)
+gg.editAll("999.9949", 16)
+gg.clearResults()
+--Speed Knock
+gg.clearResults()
+gg.setRanges(32)
+gg.searchNumber("0;7.0064923e-45;1;100;1;2,500,000,000.0;0.10000000149;88", 16, false, 536870912, 0, -1)
+gg.searchNumber("1", 16, false, 536870912, 0, -1)
+gg.getResults(100)
+gg.editAll("9999", 16)
+gg.clearResults()
+--Huga Enemy
+gg.clearResults()
+gg.setRanges(16)
+gg.clearResults()
+gg.searchNumber("-28,767,403,185,799,424;4,575,657,222,473,777,152", 32, false, 536870912, 0, -1)
+gg.searchNumber("4,575,657,222,473,777,152", 32, false)
+gg.getResults(10)
+gg.editAll("4,575,657,222,474,196,582", 32)
+gg.clearResults()
+end
+-----------------------------------------------------------------------
+--function FOOTER AND EXIT bellow
+function EXIT()
+gg.setVisible(false) --false to hide -- true to view
+--there are 15 memory range on your device
+--added 1
+--green
+gg.setRanges(
+gg.REGION_JAVA_HEAP |
+gg.REGION_C_HEAP |
+gg.REGION_C_ALLOC |
+gg.REGION_C_DATA |
+gg.REGION_C_BSS |
+gg.REGION_PPSSPP |
+gg.REGION_ANONYMOUS |
+--yellow
+gg.REGION_JAVA |
+gg.REGION_STACK |
+gg.REGION_ASHMEM |
+--yellowlight
+gg.REGION_OTHER |
+--red
+gg.REGION_BAD |
+--purple
+gg.REGION_CODE_APP |
+--purple_light
+gg.REGION_CODE_SYS |
+--added
+gg.REGION_VIDEO)
+-----------------------------------------------------------------------
+--function skipRestore for restore...
+--your setting memory saved
+gg.skipRestoreState()
+-----------------------------------------------------------------------
+--code exit from your operating system
+os.exit()
+end 
+-----------------------------------------------------------------------
+--function HOME yout can put it on top too
+function HOME()
+anythingyoucanwritehere=1
+Home1()
+end
+-----------------------------------------------------------------------
+--PH letter is your code menu script
+while(true)do
+if gg.isVisible(true) then PH=1
+gg.setVisible(false)
+end
+-----------------------------------------------------------------------
+if PH==1 then Home1() os.exit() end
+end
+-----------------------------------------------------------------------
+
+
